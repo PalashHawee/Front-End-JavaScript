472b3885e9e35a37e2cea30779e5464dd90379de
Palash Hawee <haweepalash@gmail.com>
2023-03-18 19:58:39 +0530

    Click on Map

--- OOP/script.js
+++ OOP/script.js
@@ -13,15 +13,86 @@ const Person = function (firstName, birthYear) {
 };
 
 //Instances of Person
-const palash = new Person('Paul', 1992);
-console.log(palash);
+// const palash = new Person('Paul', 1992);
+// console.log(palash);
 
-//Protypes // it inherits properties from the object
-Person.prototype.calcage = function () {
-  console.log(2037 - this.birthYear);
-};
-palash.calcage();
+// //Protypes // it inherits properties from the object
+// Person.prototype.calcage = function () {
+//   console.log(2037 - this.birthYear);
+// };
+// palash.calcage();
 
 //Adding properties to object
-Person.prototype.species = 'Homo Sapiens';
-console.log(palash.species);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(palash.species);
+
+// console.log(palash.__proto__.__proto__);
+// console.log(palash.__proto__.__proto__.__proto__); //returns null
+
+// const arr = [4, 4, 4, 7, 8, 9, 1, 2, 2, 3, 3, 5];
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+// console.log(arr.unique());
+
+//Implementing Constructor function and inheritance
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+//Implementing speed function
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
+//Implementing brake method
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
+// const bmw = new Car('bmw', 120);
+// const marcedes = new Car('Marcedes', 95);
+// bmw.accelerate();
+// bmw.brake();
+// bmw.accelerate();
+
+//Class decalrtion
+// class PersonCl {
+//   constructor(firstName, birthYear) {
+//     (this.firstName = firstName), (this.birthYear = birthYear);
+//   }
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+//   greet() {
+//     console.log(`Hello ${this.firstName}`);
+//   }
+// }
+
+// const snagma = new PersonCl('Bapon', 1992);
+// console.log(snagma);
+// snagma.calcAge();
+
+// // PersonCl.prototype.greet = function () {
+// //   console.log(`Hello ${this.firstName}`);
+// // };
+// snagma.greet();
+
+//Getters and setters for object
+const account = {
+  owner: 'Palash',
+  movements: [200, 700, 100, 500],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
---
+++ map/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//Geolocation API
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      //Copied from leaflet map library
+      const map = L.map('map').setView(coords, 10); //10 is the zoom view
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      //Implementing click event on Map
+      map.on('click', mapEvent => {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert("Couldn't get the position");
+    }
+  );
